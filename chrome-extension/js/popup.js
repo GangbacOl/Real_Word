a406f7285774cae72a9c941151b6bbdfa925f5af
GangbacOl <gangyeonjo336@gmail.com>
2020-07-26 02:30:15 +0900

    [UPD] 최근에 저장한 순서대로 3개까지 출력해주는 로직 작성.

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,1 +1,8 @@
-//test
+chrome.storage.sync.get(['words'], function (result) {
+    for (var i = result.words.length - 1; i > result.words.length - 4; i--) {
+        if (!result.words[i]) break;
+        var li = document.createElement('li');
+        li.innerText = result.words[i];
+        document.querySelector('body > div.main > div').append(li);
+    }
+});
