311082da9e6f303f4800fd3fb953296a9e3f9e25
GangbacOl <gangyeonjo336@gmail.com>
2020-07-29 17:49:57 +0900

    [UPD] 1. 변수명 변경. result -> storageData 2. 변수 생성 var -> let, const로 변경.

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -4,16 +4,16 @@
 //     });
 // });
 
-chrome.storage.sync.get(['words'], function (result) {
-    if (!result.words[0]) {
-        var li = document.createElement('span');
+chrome.storage.sync.get(['words'], (storageData) => {
+    if (!storageData.words[0]) {
+        const li = document.createElement('span');
         li.innerText = '저장한 단어가 없음.';
         document.querySelector('body > div.main > div').append(li);
     }
-    for (var i = result.words.length - 1; i > result.words.length - 4; i--) {
-        if (!result.words[i]) break;
-        var li = document.createElement('li');
-        li.innerText = result.words[i];
+    for (let i = storageData.words.length - 1; i > storageData.words.length - 4; i--) {
+        if (!storageData.words[i]) break;
+        const li = document.createElement('li');
+        li.innerText = storageData.words[i];
         document.querySelector('body > div.main > div').append(li);
     }
 });
