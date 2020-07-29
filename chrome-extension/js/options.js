c44e66a9b097dc0ffaff0560c6c11cd7b6ab5cc4
GangbacOl <gangyeonjo336@gmail.com>
2020-07-29 17:51:28 +0900

    [UPD] 1.api 서버로 storage.storage 배열 전송. 2. 번역된 단어 출력.

--- chrome-extension/js/options.js
+++ chrome-extension/js/options.js
@@ -1,17 +1,25 @@
-CLIENT_ID = 'B61JvRHm5PkonxZzru0O';
-CLIENT_SECRET = 'y6X0iowqJh';
+const sendWords = (words) => {
+    return fetch(`http://localhost:3000/translate`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            text: words,
+        }),
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            return response.translatedText;
+        })
+        .catch((err) => console.log(err));
+};
 
-fetch(`http://localhost:3000/translate`, {
-    method: 'POST',
-    text: 'apple',
-})
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
-
-chrome.storage.sync.get(['words'], function (result) {
-    for (var item in result.words) {
-        var li = document.createElement('li');
-        li.innerText = result.words[item];
+chrome.storage.sync.get(['words'], async (storageData) => {
+    const translatedWords = await sendWords(storageData.words);
+    for (var item in storageData.words) {
+        const li = document.createElement('li');
+        li.innerText = storageData.words[item] + ': ' + translatedWords[item];
         document.querySelector('body > div.main > div').append(li);
     }
 });
