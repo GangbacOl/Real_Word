a5adeeeacebddc2d6a22a0675d9002a90f042c42
GangbacOl <gangyeonjo336@gmail.com>
2020-07-30 23:42:15 +0900

    [UPD] 수정 ui 레이아웃 구성.

--- chrome-extension/js/options.js
+++ chrome-extension/js/options.js
@@ -19,7 +19,28 @@ chrome.storage.sync.get(['words'], async (storageData) => {
     const translatedWords = await sendWords(storageData.words);
     for (var item in storageData.words) {
         const li = document.createElement('li');
-        li.innerText = storageData.words[item] + ': ' + translatedWords[item];
+        const span = document.createElement('span');
+        const div = document.createElement('div');
+        const input = document.createElement('input');
+        const saveButton = document.createElement('input');
+        const cancelButton = document.createElement('input');
+        saveButton.setAttribute('type', 'button');
+        cancelButton.setAttribute('type', 'button');
+        saveButton.setAttribute('value', 'Save');
+        cancelButton.setAttribute('value', 'Cancel');
+        div.append(input, saveButton, cancelButton);
+        div.style.display = 'none';
+        span.innerText = translatedWords[item];
+        li.innerText = storageData.words[item] + ': ';
+        li.append(span, div);
         document.querySelector('body > div.main > div').append(li);
+
+        span.onclick = () => {
+            span.style.display = 'none';
+            div.style.display = 'inline-block';
+        };
     }
 });
+
+// const spans = document.getElementsByTagName('span');
+// console.log(spans);
