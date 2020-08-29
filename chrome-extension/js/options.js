bb2d315223a32fefbb79b0ba1e3f130ef010c342
GangbacOl <gangyeonjo336@gmail.com>
2020-08-29 22:12:27 +0900

    [UPD] 인터넷 연결 유무 흐름제어 로직 작성.

--- chrome-extension/js/options.js
+++ chrome-extension/js/options.js
@@ -21,41 +21,45 @@ const sendWords = (words) => {
         .catch((err) => console.log(err));
 };
 
-chrome.storage.sync.get(['wordsList'], async (storageData) => {
-    const main = document.querySelector('body > .main');
-    if (checkDayPass(storageData.wordsList[storageData.wordsList.length - 1])) {
-        console.log('good morning');
-        storageData.wordsList.unshift({
-            wordsList: [
-                {
-                    date: new Date().format('yyyy-MM-dd(KS)'),
-                    words: [],
-                },
-            ],
+if (navigator.onLine) {
+    chrome.storage.sync.get(['wordsList'], async (storageData) => {
+        const main = document.querySelector('body > .main');
+        if (checkDayPass(storageData.wordsList[storageData.wordsList.length - 1])) {
+            console.log('good morning');
+            storageData.wordsList.unshift({
+                wordsList: [
+                    {
+                        date: new Date().format('yyyy-MM-dd(KS)'),
+                        words: [],
+                    },
+                ],
+            });
+        }
+        storageData.wordsList.map(async (wordsItem) => {
+            const div = document.createElement('div');
+            div.setAttribute('class', 'main-child');
+            const h2 = document.createElement('h2');
+            h2.innerText = wordsItem.date;
+            div.append(h2);
+            const translatedWords = await sendWords(wordsItem.words);
+            wordsItem.words.map((word, index) => {
+                const li = document.createElement('li');
+                const span_en = document.createElement('span');
+                const span_ko = document.createElement('span');
+                span_en.setAttribute('class', 'span-eng');
+                span_ko.setAttribute('class', 'span-kor');
+                span_en.innerText = word;
+                span_ko.innerText = translatedWords[index];
+                li.append(span_en, ': ', span_ko);
+                div.append(li);
+                span_ko.onclick = () => {
+                    // 네이버 영어사전 페이지
+                    window.open(`https://en.dict.naver.com/#/search?query=${li.innerText.split(':')[0]}`);
+                };
+            });
+            main.append(div);
         });
-    }
-    storageData.wordsList.map(async (wordsItem) => {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'main-child');
-        const h2 = document.createElement('h2');
-        h2.innerText = wordsItem.date;
-        div.append(h2);
-        const translatedWords = await sendWords(wordsItem.words);
-        wordsItem.words.map((word, index) => {
-            const li = document.createElement('li');
-            const span_en = document.createElement('span');
-            const span_ko = document.createElement('span');
-            span_en.setAttribute('class', 'span-eng');
-            span_ko.setAttribute('class', 'span-kor');
-            span_en.innerText = word;
-            span_ko.innerText = translatedWords[index];
-            li.append(span_en, ': ', span_ko);
-            div.append(li);
-            span_ko.onclick = () => {
-                // 네이버 영어사전 페이지
-                window.open(`https://en.dict.naver.com/#/search?query=${li.innerText.split(':')[0]}`);
-            };
-        });
-        main.append(div);
     });
-});
+} else {
+    document.querySelector('body').innerText = '인터넷 연결상태를 확인해주세요.';
+}
