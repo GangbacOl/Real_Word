29acdbd279192d9379ec07879302937c1f6746b8
GangbacOl <gangyeonjo336@gmail.com>
2020-10-15 23:33:07 +0900

    [UPD] 단어 출력 로직 업데이트.

--- chrome-extension/js/options.js
+++ chrome-extension/js/options.js
@@ -1,64 +1,59 @@
-const checkDayPass = (day) => {
-    const today = new Date().format('dd');
-    if (today === day) return false;
-    else true;
-};
-
-const sendWords = (words) => {
-    return fetch(`http://localhost:3000/translate`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-            text: words,
-        }),
-    })
-        .then((response) => response.json())
-        .then((response) => {
-            return response.translatedText;
+const sendWords = async (words) => {
+    console.log('Here is sendWords method');
+    return new Promise((resolve, reject) => {
+        fetch(`http://localhost:3000/translate`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+                text: words,
+            }),
         })
-        .catch((err) => console.log(err));
+            .then((response) => response.json())
+            .then((response) => {
+                resolve(response.translatedText);
+            })
+            .catch((err) => reject(err));
+    });
 };
 
 if (navigator.onLine) {
-    chrome.storage.sync.get(['wordsList'], async (storageData) => {
+    chrome.storage.sync.get(['wordsList'], (storageData) => {
         const main = document.querySelector('body > .main');
-        if (checkDayPass(storageData.wordsList[storageData.wordsList.length - 1])) {
-            console.log('good morning');
-            storageData.wordsList.unshift({
-                wordsList: [
-                    {
-                        date: new Date().format('yyyy-MM-dd(KS)'),
-                        words: [],
-                    },
-                ],
+        // console.log(storageData.wordsList);
+        storageData.wordsList
+            .sort((a, b) => b - a)
+            .map(async (wordsItem) => {
+                console.log(wordsItem);
+                const div = document.createElement('div');
+                const h2 = document.createElement('h2');
+                const translatedWords = await sendWords(wordsItem.words);
+                console.log('awefawef');
+                // const translatedWords = 'asdf';
+
+                div.setAttribute('class', 'main-child');
+                h2.innerText = wordsItem.date;
+                div.append(h2);
+
+                wordsItem.words.map((word, index) => {
+                    const li = document.createElement('li');
+                    const span_en = document.createElement('span');
+                    const span_ko = document.createElement('span');
+                    span_en.setAttribute('class', 'span-eng');
+                    span_ko.setAttribute('class', 'span-kor');
+                    span_en.innerText = word;
+                    span_ko.innerText = translatedWords[index];
+                    li.append(span_en, ': ', span_ko);
+                    div.append(li);
+                    span_ko.onclick = () => {
+                        // 네이버 영어사전 페이지
+                        window.open(`https://en.dict.naver.com/#/search?query=${li.innerText.split(':')[0]}`);
+                    };
+                });
+                console.log(div);
+                main.append(div);
             });
-        }
-        storageData.wordsList.map(async (wordsItem) => {
-            const div = document.createElement('div');
-            div.setAttribute('class', 'main-child');
-            const h2 = document.createElement('h2');
-            h2.innerText = wordsItem.date;
-            div.append(h2);
-            const translatedWords = await sendWords(wordsItem.words);
-            wordsItem.words.map((word, index) => {
-                const li = document.createElement('li');
-                const span_en = document.createElement('span');
-                const span_ko = document.createElement('span');
-                span_en.setAttribute('class', 'span-eng');
-                span_ko.setAttribute('class', 'span-kor');
-                span_en.innerText = word;
-                span_ko.innerText = translatedWords[index];
-                li.append(span_en, ': ', span_ko);
-                div.append(li);
-                span_ko.onclick = () => {
-                    // 네이버 영어사전 페이지
-                    window.open(`https://en.dict.naver.com/#/search?query=${li.innerText.split(':')[0]}`);
-                };
-            });
-            main.append(div);
-        });
     });
 } else {
     document.querySelector('body').innerText = '인터넷 연결상태를 확인해주세요.';
