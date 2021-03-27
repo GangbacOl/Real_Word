c9a123bb809b362627b4e994783de7e3b081793e
GangbacOl <gangyeonjo336@gmail.com>
2021-03-27 10:42:34 +0900

    [UPD] chrome extension id 자동으로 가져오는 기능 추가

--- chrome-extension/js/popup.js
+++ chrome-extension/js/popup.js
@@ -1,10 +1,17 @@
+const link = document.querySelector('.footer a');
+link.href = `chrome-extension://${chrome.runtime.id}/html/options.html`;
+
 chrome.storage.sync.get(['wordsList'], (storageData) => {
     if (!storageData.wordsList[0].words[0]) {
         const li = document.createElement('span');
         li.innerText = '저장한 단어가 없음.';
         document.querySelector('body > div.main > div').append(li);
     }
-    for (let i = storageData.wordsList[0].words.length - 1; i > storageData.wordsList[0].words.length - 4; i--) {
+    for (
+        let i = storageData.wordsList[0].words.length - 1;
+        i > storageData.wordsList[0].words.length - 4;
+        i--
+    ) {
         if (!storageData.wordsList[0].words[i]) break;
         const li = document.createElement('li');
         li.innerText = storageData.wordsList[0].words[i];
